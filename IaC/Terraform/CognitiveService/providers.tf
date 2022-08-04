26c24f3dd51c9a435c39d7d18f98a6096693b993
Erick Ramírez <erramire@microsoft.com>
2022-08-03 21:17:38 -0500

    state configuration

--- IaC/Terraform/CognitiveService/providers.tf
+++ IaC/Terraform/CognitiveService/providers.tf
@@ -1,11 +1,18 @@
 terraform {
 
   required_version = ">=0.12"
-  
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "personalexpenses-tfstate-rg"
+    storage_account_name = "persexptfstatesa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
